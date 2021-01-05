32dfe33b0b8de204391cd352e6b8e96ab6df4aff
CrabDumplings <56217627+CrabDumplings@users.noreply.github.com>
2021-01-05 17:13:38 +0800

    二叉搜索树的最近公共祖先

--- src/Tree/LowestCommonAncestor.java
+++ src/Tree/LowestCommonAncestor.java
@@ -3,10 +3,10 @@ class LowestCommonAncestor {
         // root 是p,q的最近公共祖先
         // 节点root为空时跳出
         while(root != null) {
-            if(root.val < p.val && root.val < q.val) // p,q 都在 root 的右子树中
-                root = root.right;                   // 遍历至右子节点
-            else if(root.val > p.val && root.val > q.val) // p,q 都在 root 的左子树中
-                root = root.left;                         // 遍历至左子节点
+            if(root.val < p.val && root.val < q.val)       // p,q 都在 root 的右子树中
+                root = root.right;                         // 遍历至右子节点
+            else if(root.val > p.val && root.val > q.val)  // p,q 都在 root 的左子树中
+                root = root.left;                          // 遍历至左子节点
             else break;
         }
         return root;
