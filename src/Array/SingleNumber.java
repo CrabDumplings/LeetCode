85680a25a6bad6524f5fdc0691e0f0569d6471e3
CrabDumplings <56217627+CrabDumplings@users.noreply.github.com>
2021-01-25 22:02:00 +0800

    数组中数字出现的次数

---
+++ src/Array/SingleNumber.java
@@ -0,0 +1,17 @@
+class SingleNumber {
+    public int solution(int[] nums) {
+        int[] counts = new int[32];
+        for(int num : nums) {
+            for(int j = 0; j < 32; j++) {
+                counts[j] += num & 1;
+                num >>>= 1;
+            }
+        }
+        int res = 0, m = 3;
+        for(int i = 0; i < 32; i++) {
+            res <<= 1;
+            res |= counts[31 - i] % m;
+        }
+        return res;
+    }
+}
